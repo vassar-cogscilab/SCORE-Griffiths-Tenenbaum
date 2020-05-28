c68fa785af8ab7597a872e759e20f6b7ed907ff5
tcharles-vassar <47159329+tcharles-vassar@users.noreply.github.com>
2020-05-28 15:48:29 -0400

    Updated Fake Data Script

--- Analysis/analysis.R
+++ Analysis/analysis.R
@@ -1,30 +1,22 @@
 library(tidyverse)
 
-DF1 <- data.frame(category = c("teacake", "toxin"),
-                   question1 = seq(1, 34, 1),
-                   question2 = seq(1, 34, 1),
-                   question3 = seq(1, 34, 1))
+DF1 <- data.frame(category = sample(c("teacake", "toxin"), size = 14, replace = T),
+                   question1 = sample(1:34, size = 14, replace = T),
+                   question2 = sample(1:34, size = 14, replace = T),
+                   question3 = sample(1:34, size = 14, replace = T))
 
 r <- sample(nrow(DF1))
 DF1 <- DF1[r, 1:4]
-head(DF1, n = 40)
+head(DF1, n = 14)
 
-DF2 <- data.frame(category = c("train", "taxicab"),
-                  question1 = seq(1, 103, 1),
-                  question2 = seq(1, 103, 1),
-                  question3 = seq(1, 103, 1))
+DF2 <- data.frame(category = sample(c("train", "taxicab"), size = 15, replace = T),
+                  question1 = sample(1:103, size = 15, replace = T),
+                  question2 = sample(1:103, size = 15, replace = T),
+                  question3 = sample(1:103, size = 15, replace = T))
 
 r <- sample(nrow(DF2))
 DF2 <- DF2[r, 1:4]
-head(DF2, n = 41)
+head(DF2, n = 15)
 
-DF3 <- data.frame(category = c("train", "taxicab"),
-                   question1 = seq(1, 103, 1),
-                   question2 = seq(1, 103, 1),
-                   question3 = seq(1, 103, 1))
-
-r <- sample(nrow(DF3))
-DF3 <- DF3[r, 1:4]
-head(DF3, n = 41)
 
 total <- rbind(DF1, DF2)
