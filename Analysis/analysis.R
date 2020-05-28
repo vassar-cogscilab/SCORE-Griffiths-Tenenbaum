8d03d62b79a9b8ef81160a40c44883bd16093547
tcharles-vassar <47159329+tcharles-vassar@users.noreply.github.com>
2020-05-28 14:56:36 -0400

    Problematic Fake Data Update Code

--- Analysis/analysis.R
+++ Analysis/analysis.R
@@ -1,9 +1,30 @@
 library(tidyverse)
-data <- expand.grid(category = c("teacake", "train", "toxin", "taxicab"),
-                    question1 = seq(1, 120, 1),
-                    question2 = seq(1, 120, 1),
-                    question3 = seq(1, 120, 1))
-r <- sample(nrow(data))
-data <- data[r, 1:4]
-head(data, n = 81)
 
+DF1 <- data.frame(category = c("teacake", "toxin"),
+                   question1 = seq(1, 34, 1),
+                   question2 = seq(1, 34, 1),
+                   question3 = seq(1, 34, 1))
+
+r <- sample(nrow(DF1))
+DF1 <- DF1[r, 1:4]
+head(DF1, n = 40)
+
+DF2 <- data.frame(category = c("train", "taxicab"),
+                  question1 = seq(1, 103, 1),
+                  question2 = seq(1, 103, 1),
+                  question3 = seq(1, 103, 1))
+
+r <- sample(nrow(DF2))
+DF2 <- DF2[r, 1:4]
+head(DF2, n = 41)
+
+DF3 <- data.frame(category = c("train", "taxicab"),
+                   question1 = seq(1, 103, 1),
+                   question2 = seq(1, 103, 1),
+                   question3 = seq(1, 103, 1))
+
+r <- sample(nrow(DF3))
+DF3 <- DF3[r, 1:4]
+head(DF3, n = 41)
+
+total <- rbind(DF1, DF2)
