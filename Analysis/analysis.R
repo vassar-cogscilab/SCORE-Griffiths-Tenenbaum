b03edd18561027123bff2012d537b367a052d819
tcharles-vassar <47159329+tcharles-vassar@users.noreply.github.com>
2020-05-29 15:32:20 -0400

    Inclusion Criteria (Not Working Yet)

--- Analysis/analysis.R
+++ Analysis/analysis.R
@@ -1,18 +1,18 @@
 library(tidyverse)
 
 DF1 <- data.frame(category = sample(c("teacake", "toxin"), size = 14, replace = T),
-                   question1 = sample(1:34, size = 14, replace = T),
-                   question2 = sample(1:34, size = 14, replace = T),
-                   question3 = sample(1:34, size = 14, replace = T))
+                   question1 = sample(1:100, size = 14, replace = T),
+                   question2 = sample(1:100, size = 14, replace = T),
+                   question3 = sample(1:100, size = 14, replace = T))
 
 r <- sample(nrow(DF1))
 DF1 <- DF1[r, 1:4]
 head(DF1, n = 14)
 
 DF2 <- data.frame(category = sample(c("train", "taxicab"), size = 15, replace = T),
-                  question1 = sample(1:103, size = 15, replace = T),
-                  question2 = sample(1:103, size = 15, replace = T),
-                  question3 = sample(1:103, size = 15, replace = T))
+                  question1 = sample(50:200, size = 15, replace = T),
+                  question2 = sample(50:200, size = 15, replace = T),
+                  question3 = sample(50:200, size = 15, replace = T))
 
 r <- sample(nrow(DF2))
 DF2 <- DF2[r, 1:4]
@@ -22,4 +22,4 @@ head(DF2, n = 15)
 total <- rbind(DF1, DF2)
 print(total)
 
-write.csv(total, "C:\\Documents\\GitHub\\SCORE-Griffiths-Tenenbaum\\Analysis", row.names = F)
+write.csv(total, "fake_data", row.names = F)
--- Analysis/analysis_script.R
+++ Analysis/analysis_script.R
@@ -3,10 +3,55 @@ library(tidyverse)
 ## We will be loading in the raw data from the experiment, which will not need to be
 ## filtered besides by the inclusion criteria which will be described below
 
-all.data <- read_csv(fake_data.csv)
+all.data <- read_csv('fake_data')
 
 ## Inclusion Criteria
 ## Inclusion requires that:
 ## Responses must be greater than tpast
-## Responses must be between 3 standard deviations from the mean
+## Responses must be within 3 standard deviations from the mean
 
+##group.mean <- all.data %>%
+ ## all.data[abs(all.data$question1 - mean(all.data$question1)) / sd(all.data$question1) <= 3,] %>%
+ ## all.data[abs(all.data$question2 - mean(all.data$question2)) / sd(all.data$question2) <= 3,] %>%
+ ## all.data[abs(all.data$question3 - mean(all.data$question3)) / sd(all.data$question3) <= 3,]
+
+question1.answer <- all.data[!(abs(all.data$question1 - mean(all.data$question1)) / sd(all.data$question1)) >3,]
+
+question2.answer <- all.data[!(abs(all.data$question2 - mean(all.data$question2)) / sd(all.data$question2)) >3,]
+
+question3.answer <- all.data[!(abs(all.data$question3 - mean(all.data$question3)) / sd(all.data$question3)) >3,]
+
+tea.inclusion <- all.data %>%
+  filter(category=="teacake") %>%
+  summarize(tea.answers = all.data$question1 && all.data$question2 && all.data$question3) %>%
+  mutate(tea.include = tea.answers >= 34)
+
+toxin.inclusion <- all.data %>%
+  filter(category == "toxin") %>%
+  summarize(toxin.answers = all.data$question1 && all.data$question2 && all.data$question3) %>%
+  mutate(toxin.include = toxin.answers >= 34)
+
+train.inclusion <- all.data %>%
+  filter(category == "train") %>%
+  summarize(train.answers = all.data$question1 && all.data$question2 && all.data$question3) %>%
+  mutate(train.include = train.answers >= 103)
+
+taxi.inclusion <- all.data %>%
+  filter(category == "taxicab") %>%
+  summarize(taxi.answers = all.data$question1 && all.data$question2 && all.data$question3) %>%
+  mutate(taxi.include = taxi.answers >= 103)
+
+final.include <- all.data %>%
+  mutate(include = tea.include && toxin.include && train.include && taxi.include)
+
+use.participants <- final.include %>%
+  filter(include == T) %>%
+  select(category)
+
+## Now we apply these inclusion criterias
+
+filtered.data <- all.data %>%
+  filter(category == use.participants)
+
+print(tea.include)
+  
