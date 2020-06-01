4974987e1185d0d52ebfc686a4b896f1cf35dc55
tcharles-vassar <47159329+tcharles-vassar@users.noreply.github.com>
2020-06-01 14:43:28 -0400

    Update ANOVA

--- Analysis/analysis_script.R
+++ Analysis/analysis_script.R
@@ -47,33 +47,53 @@ final.include <- rbind(tea.inclusion,
                        taxi.inclusion)
 
 ## We now need to transform the data to the form t/tpast
-tea.toxin.transform <- final.include %>%
-  filter(category == "teacake" | category == "toxin") %>%
+tea.transform <- final.include %>%
+  filter(category == "teacake") %>%
   mutate(question1.transform = question1/34,
          question2.transform = question2/34,
          question3.transform = question3/34)
 
-train.taxi.transform <- final.include %>%
-  filter(category == "train" | category == "taxicab") %>%
+toxin.transform <- final.include %>%
+  filter(category == "toxin") %>%
+  mutate(question1.transform = question1/34,
+         question2.transform = question2/34,
+         question3.transform = question3/34)
+
+train.transform <- final.include %>%
+  filter(category == "train") %>%
+  mutate(question1.transform = question1/103,
+         question2.transform = question2/103,
+         question3.transform = question3/103)
+
+taxi.transform <- final.include %>%
+  filter(category == "taxi") %>%
   mutate(question1.transform = question1/103,
          question2.transform = question2/103,
          question3.transform = question3/103)
 
 ## Create a data frame with the new transformed data added in
-final.transform <- rbind(tea.toxin.transform, train.taxi.transform)
+final.transform <- rbind(tea.transform, toxin.transform, train.transform, taxi.transform)
 
 ## Now we need to code for the ANOVA test
-question1.results <- aov(question1.transform ~ category, 
-      data = final.transform)
+final.transform$category <-factor(final.transform$category, 
+                                  levels = c("teacake", "toxin", "train", "taxicab"))
 
-summary(question1.results)
+formulae <- lapply(colnames(final.transform) [5:ncol(final.transform)],
+                   function(x) as.formula(paste0(x, "~ category")))
 
-question2.results <- aov(question2.transform ~ category, 
-                         data = final.transform)
+lapply(formulae, function(x) summary(aov(x, data = final.transform)))
 
-summary(question2.results)
+##question1.results <- aov(question1.transform ~ category, 
+      ##data = final.transform)
 
-question3.results <- aov(question3.transform ~ category, 
-                         data = final.transform)
+##summary(question1.results)
 
-summary(question3.results)
+##question2.results <- aov(question2.transform ~ category, 
+                         ##data = final.transform)
+
+##summary(question2.results)
+
+##question3.results <- aov(question3.transform ~ category, 
+                         ##data = final.transform)
+
+##summary(question3.results)
