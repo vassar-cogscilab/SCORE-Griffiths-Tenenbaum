d50fc34f291cf3dea9b38db7c339ecd292ecbf63
tcharles-vassar <47159329+tcharles-vassar@users.noreply.github.com>
2020-06-01 11:51:45 -0400

    Data Transformation

--- Analysis/analysis_script.R
+++ Analysis/analysis_script.R
@@ -3,7 +3,7 @@ library(tidyverse)
 ## We will be loading in the raw data from the experiment, which will not need to be
 ## filtered besides by the inclusion criteria which will be described below
 
-all.data <- read_csv('fake_data')
+all.data <- read_csv('fake_data.csv')
 
 ## Inclusion Criteria
 ## Inclusion requires that:
@@ -17,37 +17,48 @@ question2.answer <- all.data[!(abs(all.data$question2 - mean(all.data$question2)
 question3.answer <- all.data[!(abs(all.data$question3 - mean(all.data$question3)) / sd(all.data$question3)) >3,]
 
 tea.inclusion <- all.data %>%
-  filter(category=="teacake", question1 >= 34, question2 >= 34, question3 >= 34) %>%
-  ##summarize(tea.answers = all.data$question1 && all.data$question2 && all.data$question3) %>%
-  ##mutate(goober = question3 >= 34)
+  filter(category=="teacake", 
+         question1 >= 34, 
+         question2 >= 34, 
+         question3 >= 34)
 
 toxin.inclusion <- all.data %>%
-  filter(category == "toxin", question1 >= 34, question2 >= 34, question3 >= 34) %>%
-  ##summarize(toxin.answers = all.data$question1 && all.data$question2 && all.data$question3) %>%
-  ##mutate(toxin.include = toxin.answers >= 34)
+  filter(category == "toxin", 
+         question1 >= 34, 
+         question2 >= 34, 
+         question3 >= 34)
 
 train.inclusion <- all.data %>%
-  filter(category == "train", question1 >= 103, question2 >= 103, question3 >= 103) %>%
-  ##summarize(train.answers = all.data$question1 && all.data$question2 && all.data$question3) %>%
-  ##mutate(train.include = train.answers >= 103)
+  filter(category == "train", 
+         question1 >= 103, 
+         question2 >= 103, 
+         question3 >= 103)
 
 taxi.inclusion <- all.data %>%
-  filter(category == "taxicab", question1 >= 103, question2 >= 103, question3 >= 103) %>%
-  ##summarize(taxi.answers = all.data$question1 && all.data$question2 && all.data$question3) %>%
-  ##mutate(taxi.include = taxi.answers >= 103)
+  filter(category == "taxicab", 
+         question1 >= 103, 
+         question2 >= 103, 
+         question3 >= 103)
 
-final.include <- all.data %>%
-  rbind(tea.inclusion, toxin.inclusion, train.inclusion, taxi.inclusion)
-  ##mutate(include = tea.include && toxin.include && train.include && taxi.include)
+## Now we apply the inclusion criteria and create an updated data frame
+final.include <- rbind(tea.inclusion, 
+                       toxin.inclusion, 
+                       train.inclusion, 
+                       taxi.inclusion)
 
-use.participants <- final.include %>%
-  filter(question1.answer == T, question2.answer == T, question3.answer == T) %>%
-  select(category)
+## We now need to transform the data to the form t/tpast
+tea.toxin.transform <- final.include %>%
+  filter(category == "teacake" | category == "toxin") %>%
+  mutate(question1.transform = question1/34,
+         question2.transform = question2/34,
+         question3.transform = question3/34)
 
-## Now we apply these inclusion criterias
+train.taxi.transform <- final.include %>%
+  filter(category == "train" | category == "taxicab") %>%
+  mutate(question1.transform = question1/103,
+         question2.transform = question2/103,
+         question3.transform = question3/103)
 
-filtered.data <- all.data %>%
-  filter(category == use.participants)
+final.transform <- rbind(tea.toxin.transform, train.taxi.transform)
 
-print(tea.include)
-  
+
