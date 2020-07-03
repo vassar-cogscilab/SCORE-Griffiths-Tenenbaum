c58323c892296a9527cd08b6c29ba59cfc6a6536
tcharles-vassar <47159329+tcharles-vassar@users.noreply.github.com>
2020-07-03 08:30:02 -0400

    Process Data With Age Information

---
+++ Analysis/process_data.R
@@ -0,0 +1,16 @@
+library(jsonlite)
+library(tidyverse)
+
+json_data <- fromJSON("test_fake_data.json")
+
+converted_data = subset(json_data, select = -c(row, rt, stimulus, button_pressed,
+                                               trial_type, trial_index, time_elapsed))
+
+minusRow <- converted_data[-c(1),]%>%
+  unnest(cols = c(responses))%>%
+  mutate(answer=str_extract(str_extract(responses,":.*"),"[0-9]+"),
+         responses=NULL)
+
+
+Nth.delete<-function(dataframe, n)dataframe[-(seq(n,to=nrow(dataframe),by=n)),]
+Nth.delete(minusRow, 5)
